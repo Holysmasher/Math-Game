10afa3fa3634447fc111dbbb58ae66d9a76b0375
Holysmasher <shawnsummers@live.com>
2025-12-09 22:54:26 -0700

    Refactored into Menu function

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -3,38 +3,7 @@
 var name = Console.ReadLine();
 var date = DateTime.Now;
 
-Console.WriteLine("-------------------------------");
-Console.WriteLine($"Hello, {name}. It's {date.DayOfWeek}. This is your math game.");
-Console.WriteLine($@"What game would you like to play today? Choose from the options below:
-A - Addition
-S - Subtraction
-M - Multiplication
-D - Division
-Q - Quit the program");
-Console.WriteLine("-------------------------------");
-var gameSelected = Console.ReadLine();
-
-switch (gameSelected.Trim().ToUpper())
-{ 
-    case "A":
-        AdditionGame("You have selected the Addition Game.");
-        break;
-    case "S":
-        SubtractionGame("You have selected the Subtraction Game.");
-        break;
-    case "M":
-        MultiplicationGame("You have selected the Multiplication Game.");
-        break;
-    case "D":
-        DivisionGame("You have selected the Division Game.");
-        break;
-    case "Q":
-        Console.WriteLine("Thank you for playing. Goodbye!");
-        break;
-    default:
-        Console.WriteLine("Invalid selection. Please restart the program and choose a valid option.");
-        break;
-}
+Menu(name, date);
 
 void DivisionGame(string message)
 {
@@ -54,4 +23,40 @@ void SubtractionGame(string message)
 void AdditionGame(string message)
 {
     Console.WriteLine(message);
+}
+
+void Menu(string? name, DateTime date)
+{
+    Console.WriteLine("-------------------------------");
+    Console.WriteLine($"Hello, {name}. It's {date.DayOfWeek}. This is your math game.");
+    Console.WriteLine($@"What game would you like to play today? Choose from the options below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+    Console.WriteLine("-------------------------------");
+    var gameSelected = Console.ReadLine();
+
+    switch (gameSelected.Trim().ToUpper())
+    {
+        case "A":
+            AdditionGame("You have selected the Addition Game.");
+            break;
+        case "S":
+            SubtractionGame("You have selected the Subtraction Game.");
+            break;
+        case "M":
+            MultiplicationGame("You have selected the Multiplication Game.");
+            break;
+        case "D":
+            DivisionGame("You have selected the Division Game.");
+            break;
+        case "Q":
+            Console.WriteLine("Thank you for playing. Goodbye!");
+            break;
+        default:
+            Console.WriteLine("Invalid selection. Please restart the program and choose a valid option.");
+            break;
+    }
 }
