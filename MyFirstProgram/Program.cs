df6ed1185dc7e60769242bd702e644a54eb72f95
Holysmasher <shawnsummers@live.com>
2025-12-09 22:49:10 -0700

    Added functions for each game

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -4,7 +4,7 @@ var name = Console.ReadLine();
 var date = DateTime.Now;
 
 Console.WriteLine("-------------------------------");
-Console.WriteLine($"Hello, {name.ToUpper()}. It's {date.DayOfWeek}. This is your math game.");
+Console.WriteLine($"Hello, {name}. It's {date.DayOfWeek}. This is your math game.");
 Console.WriteLine($@"What game would you like to play today? Choose from the options below:
 A - Addition
 S - Subtraction
@@ -16,19 +16,19 @@ var gameSelected = Console.ReadLine();
 
 if (gameSelected.ToUpper().Trim() == "A")
 {
-    Console.WriteLine("You chose Addition!");
+    AdditionGame("You chose Addition");
 }
 else if (gameSelected.ToUpper().Trim() == "S")
 {
-    Console.WriteLine("You chose Subtraction!");
+    SubtractionGame("You chose Subtraction");
 }
 else if (gameSelected.ToUpper().Trim() == "M")
 {
-    Console.WriteLine("You chose Multiplication!");
+    MultiplicationGame("You chose Multiplication");
 }
 else if (gameSelected.ToUpper().Trim() == "D")
 {
-    Console.WriteLine("You chose Division!");
+    DivisionGame("You chose Division");
 }
 else if (gameSelected.ToUpper().Trim() == "Q")
 {
@@ -38,4 +38,24 @@ else if (gameSelected.ToUpper().Trim() == "Q")
 else
 {
     Console.WriteLine("Invalid choice. Please restart the program and choose a valid option.");
+}
+
+void DivisionGame(string message)
+{
+    Console.WriteLine(message);
+}
+
+void MultiplicationGame(string message)
+{
+    Console.WriteLine(message);
+}
+
+void SubtractionGame(string message)
+{
+    Console.WriteLine(message);
+}
+
+void AdditionGame(string message)
+{
+    Console.WriteLine(message);
 }
