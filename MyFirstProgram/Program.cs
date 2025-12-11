cdb55a56a7aa21b4710ea5e1ebbef3e047529dbd
Holysmasher <shawnsummers@live.com>
2025-12-11 11:56:45 -0700

    Set up Division Game

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,4 +1,6 @@
-﻿var date = DateTime.Now;
+﻿using System.Threading.Tasks.Sources;
+
+var date = DateTime.Now;
 string name = GetName();
 
 
@@ -6,7 +8,31 @@ Menu(name, date);
 
 void DivisionGame(string message)
 {
-    Console.WriteLine("Sorry, this is still a work in progress. Please select another game.");
+    var score = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        var divisionNumbers = GetDivisionNumbers();
+        var firstNumber = divisionNumbers[0];
+        var secondNumber = divisionNumbers[1];
+
+        Console.WriteLine($"{firstNumber} / {secondNumber}");
+        var result = Console.ReadLine();
+        var sum = firstNumber / secondNumber;
+        if (int.Parse(result) == sum)
+        {
+            Console.WriteLine("Correct! Well done.");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine($"Incorrect. The correct answer is {sum}.");
+        }
+
+        if (i == 4)
+        {
+            Console.WriteLine($"Game over! Your final score is {score}.");
+        }
+    }
 }
 
 void MultiplicationGame(string message)
@@ -157,4 +183,26 @@ string GetName()
 
     var name = Console.ReadLine();
     return name;
+}
+
+int[] GetDivisionNumbers()
+{
+    var random = new Random();
+    var firstNumber = random.Next(1, 99);
+    var secondNumber = random.Next(1, 99);
+
+    var result = new int[2];
+
+    while (firstNumber % secondNumber != 0)
+    {
+        firstNumber = random.Next(1, 99);
+        secondNumber = random.Next(1, 99);
+        result[0] = firstNumber;
+        result[1] = secondNumber;
+    }
+
+    result[0] = firstNumber;
+    result[1] = secondNumber;
+
+    return result;
 }
